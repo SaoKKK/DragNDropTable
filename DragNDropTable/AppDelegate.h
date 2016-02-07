ebadb6e45570db0bb3ae257cd28b586c81f49f32
Saori Kohno <saorikohno@yahoo.com>
2016-02-08 06:40:20 +0900

    Initial Commit

---
+++ DragNDropTable/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DragNDropTable
+//
+//  Created by 河野 さおり on 2016/02/08.
+//  Copyright © 2016年 河野 さおり. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ DragNDropTable/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  DragNDropTable
+//
+//  Created by 河野 さおり on 2016/02/08.
+//  Copyright © 2016年 河野 さおり. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
